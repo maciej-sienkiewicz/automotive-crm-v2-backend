2a5945e60a74da16e3889acd920d209ffe555d70
MaciejSienkiewicz <kontakt@sienkiewicz-maciej.pl>
2026-01-17 17:23:47 +0100

    fix

--- src/main/kotlin/pl/detailing/crm/visit/comment/UpdateVisitCommentHandler.kt
+++ src/main/kotlin/pl/detailing/crm/visit/comment/UpdateVisitCommentHandler.kt
@@ -1,5 +1,6 @@
 package pl.detailing.crm.visit.comment
 
+import org.apache.coyote.BadRequestException
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import pl.detailing.crm.shared.*
