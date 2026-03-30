e58aae9d090f4c6d0c8334f1de2c4431ace3dd80
MaciejSienkiewicz <kontakt@sienkiewicz-maciej.pl>
2026-03-30 19:37:40 +0200

    fix

--- src/main/kotlin/pl/detailing/crm/appointment/infrastructure/AppointmentRepository.kt
+++ src/main/kotlin/pl/detailing/crm/appointment/infrastructure/AppointmentRepository.kt
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 import java.time.Instant
+import java.time.LocalDate
 import java.util.UUID
 
 @Repository
