0dd83fade00952bc4af7b564a1d3f9d4945e322e
MaciejSienkiewicz <kontakt@sienkiewicz-maciej.pl>
2026-06-09 15:24:28 +0200

    fix

--- src/main/kotlin/pl/detailing/crm/instagram/infrastructure/RapidApiInstagramClient.kt
+++ src/main/kotlin/pl/detailing/crm/instagram/infrastructure/RapidApiInstagramClient.kt
@@ -222,7 +222,7 @@ class RapidApiInstagramClient(
      * @throws RapidApiException gdy API zwraca błąd HTTP
      */
     fun fetchUserDetails(username: String): RawInstagramUserDetails? {
-        if (!setOf("carspa.official", "carslab_pl", "carartdetailing").contains(username)) {
+        if (!setOf("carspa.official", "carslab_pl", "carartdetailing", "detailingcentrum_poznan", "fineauto.pl").contains(username)) {
             return null
         }
 
